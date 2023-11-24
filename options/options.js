155b60f8227f85729e51e8d2dd330bdfd4161b9c
Crowdbotics <team@crowdbotics.com>
2023-11-24 13:29:42 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "shiny-bread-44741",
+  url: "https://shiny-bread-44741.botics.co",
+  api: "https://shiny-bread-44741.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
